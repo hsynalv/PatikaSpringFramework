75983856b23cf0fbe225cd48a33c7aaab85b3b49
hhsynalv <hhsynalv@gmail.com>
2022-07-05 01:08:42 +0300

    Spring Framework Architecture Success

---
+++ src/main/java/com/beyazskorsky/JsfBean.java
@@ -0,0 +1,20 @@
+package com.beyazskorsky;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named(value = "cdiBean")
+@RequestScoped
+@Getter @Setter
+public class JsfBean {
+
+    private String patika;
+
+    public JsfBean() {
+        patika="Spring Boot Eğitimi";
+    }
+}
+
